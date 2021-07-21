a395db32b62214cd43ec48c3a9881c9e83a607f0
Dmitry <9984398@gmail.com>
2021-07-21 17:44:13 +0200

    New functions Declared.

--- tcalc.py
+++ tcalc.py
@@ -5,8 +5,18 @@ You have to suply file named triplog.csv , with next structure: Step in date , S
 Date format  : day.month.fullYear 
 """
 from datetime import datetime
+from typing import List, Tuples
+
 
 # TODO: Realize file reasding from CSV File format . May be add .xls format reading.
+
+
+def data_from_csv (f_name : str = "triplog.csv"):
+    pass
+
+def get_trip_days():
+    pass
+
 
 if __name__ == "__main__":
     date_in = "28.11.2020"
