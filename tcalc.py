26ed8226ccae617f8222526ea5c8b4e4e018d0f2
Dmitry <9984398@gmail.com>
2021-07-21 16:09:10 +0200

    Date format: Year changed to full format.

--- tcalc.py
+++ tcalc.py
@@ -7,14 +7,14 @@ Date format  : day.month.fullYear
 from datetime import datetime 
 
 if __name__ == '__main__':
-    date_in = '28.11.20'
-    date_out ='09.05.21'
+    date_in = '28.11.2020'
+    date_out ='09.05.2021'
 
-    date_in2 = '17.06.21'
-    date_out2 = '07.07.21'
+    date_in2 = '17.06.2021'
+    date_out2 = '07.07.2021'
 
-    trip1 =  datetime.strptime(date_out, "%d.%m.%y") - datetime.strptime(date_in, "%d.%m.%y")
-    trip2 =  datetime.strptime(date_out2, "%d.%m.%y") - datetime.strptime(date_in2, "%d.%m.%y")
+    trip1 =  datetime.strptime(date_out, "%d.%m.%Y") - datetime.strptime(date_in, "%d.%m.%Y")
+    trip2 =  datetime.strptime(date_out2, "%d.%m.%Y") - datetime.strptime(date_in2, "%d.%m.%Y")
 
     total = trip1 + trip2
     
