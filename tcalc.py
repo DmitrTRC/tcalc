4b43554a04ac9cc2641839b0a2198b46fc9d9074
Dmitry <9984398@gmail.com>
2021-07-21 17:36:16 +0200

    Formated.

--- tcalc.py
+++ tcalc.py
@@ -4,19 +4,23 @@ This script  alows automaticaly calculate available resident periods
 You have to suply file named triplog.csv , with next structure: Step in date , Step  out date
 Date format  : day.month.fullYear 
 """
-from datetime import datetime 
+from datetime import datetime
 
-if __name__ == '__main__':
-    date_in = '28.11.2020'
-    date_out ='09.05.2021'
 
-    date_in2 = '17.06.2021'
-    date_out2 = '07.07.2021'
+if __name__ == "__main__":
+    date_in = "28.11.2020"
+    date_out = "09.05.2021"
 
-    trip1 =  datetime.strptime(date_out, "%d.%m.%Y") - datetime.strptime(date_in, "%d.%m.%Y")
-    trip2 =  datetime.strptime(date_out2, "%d.%m.%Y") - datetime.strptime(date_in2, "%d.%m.%Y")
+    date_in2 = "17.06.2021"
+    date_out2 = "07.07.2021"
+
+    trip1 = datetime.strptime(date_out, "%d.%m.%Y") - datetime.strptime(
+        date_in, "%d.%m.%Y"
+    )
+    trip2 = datetime.strptime(date_out2, "%d.%m.%Y") - datetime.strptime(
+        date_in2, "%d.%m.%Y"
+    )
 
     total = trip1 + trip2
-    
-    print (total.days)
-    
+
+    print(total.days)
